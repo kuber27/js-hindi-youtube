476e6628c0ccb97ffd2f943d48735753796d75bc
Kuber <kubersingh605@gmail.com>
2025-12-25 08:18:06 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 144553
+let accontEmail = "kubersingh209@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountID = 45 
+accountEmail = "naincy605@gmail.com"
+accountPassword = "9237854"
+accountCity = "Bangaluru"
+
+//console.log(accountID);
+/*
+ hello kuber singh, how are you
+*/
+console.table([accountID, accountPassword, accountCity, accountEmail])
