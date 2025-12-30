6cb2e47942642161498c3bf82245779121dc8d73
Kuber <kubersingh605@gmail.com>
2025-12-30 09:12:39 +0000

    comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,9 @@
+// comparisions 
+
+// avoide these kind of code 
+console.log(null > 0) // false
+console.log(null == 0) // false 
+console.log(null >= 0) // true
+
+// Good method for comparisions 
+console.log("2" === 2);
